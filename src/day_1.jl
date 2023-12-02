3d5d231f4a41501944a96cd394f196d993650676
sitegui <sitegui@sitegui.com.br>
2023-12-02 11:14:33 +0100

    Solve day 1

--- src/day_1.jl
+++ src/day_1.jl
@@ -4,3 +4,26 @@ day_1:
 - Author: sitegui
 - Date: 2023-12-02
 =#
+
+file_contents = readlines("data/day_1")
+total_value = 0
+for row in file_contents
+    first_digit = nothing
+    last_digit = nothing
+    for char in row
+        if isdigit(char)
+            last_digit = parse(Int, char)
+
+            if first_digit == nothing
+                first_digit = parse(Int, char)
+            end
+        end
+    end
+
+    row_value = 10 * first_digit + last_digit
+    println(row_value)
+
+    global total_value += row_value
+end
+
+println("Total value is: ", total_value)
